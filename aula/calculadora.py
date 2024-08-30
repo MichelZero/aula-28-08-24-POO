66be0bd9ab675bc9ef81c4a81cd1956f6dc9e774
Michel <106674127+MichelZero@users.noreply.github.com>
2024-08-30 08:05:29 -0300

    up

--- aula/calculadora.py
+++ aula/calculadora.py
@@ -4,13 +4,13 @@
 
 #Crie uma classe para somar 2 valores.
 
-##### 1 etapa ###########
-class Calculadora:
-  valor1 = 0
+##### 1 teste ###########
+class Calculadora: 
+  valor1 = 0 # atributo da classe Calculadora
   valor2 = 0
   
-  # def olaMundo(self):
+  # def olaMundo(self): # método da classe Calculadora
   #   print("Olá Mundo!")
   
-  def somar(self):
-    print(f"{self.valor1}+{self.valor2}={self.valor1 + self.valor2}")
+  def somar(self): # método da classe Calculadora
+    print(f"{self.valor1}+{self.valor2}={self.valor1 + self.valor2}") # self é uma referência ao objeto que está chamando o método
--- main.py
+++ main.py
@@ -3,14 +3,15 @@
 # data: 28-08-2024
 
 # importar a classe Calculadora
-from aula.calculadora import Calculadora
+# veja que o import é feito a partir do pacote (diretório) aula
+from aula.calculadora import Calculadora # importa a classe Calculadora do arquivo calculadora.py
 
-# instancia
+# instancia a classe Calculadora e atribui a variável c1
 c1 = Calculadora()
 
-c1.valor1 = 2
+c1.valor1 = 2 # atribui 2 ao atributo valor1 do objeto c1
 c1.valor2 = 3
-soma = c1.valor1 + c1.valor2
-print(f"{c1.valor1}+{c1.valor2}={soma}")
+soma = c1.valor1 + c1.valor2 # soma os atributos valor1 e valor2 do objeto c1 
+print(f"{c1.valor1}+{c1.valor2}={soma}") # imprime a soma dos atributos valor1 e valor2 do objeto c1
 
-c1.somar()
+c1.somar() # chama o método somar do objeto c1 que imprime a soma dos atributos valor1 e valor2 do objeto c1
